3d571e996f21ebb7dbb3a8235dd5b0fda53d5087
vasilportey <portey@gmail.com>
2016-08-26 14:02:37 +0300

    fix

---
+++ DependencyInjection/Compiler/GraphQlCompilerPass.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Youshido\GraphQLBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Date: 25.08.16
+ *
+ * @author Portey Vasil <portey@gmail.com>
+ */
+class GraphQlCompilerPass implements CompilerPassInterface
+{
+
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $definition = new Definition();
+        $definition->setSynthetic(true);
+        $container->setDefinition('youshido.graphql.schema', $definition);
+
+        if ($loggerAlias = $container->getParameter('youshido.graphql.logger')) {
+            if (strpos($loggerAlias, '@') === 0) {
+                $loggerAlias = substr($loggerAlias, 1);
+            }
+
+            if (!$container->has($loggerAlias)) {
+                throw new \RuntimeException(sprintf('Logger "%s" not found', $loggerAlias));
+            }
+
+            $container->getDefinition('youshido.graphql.processor')->addMethodCall('setLogger', [new Reference($loggerAlias)]);
+        }
+    }
+}
