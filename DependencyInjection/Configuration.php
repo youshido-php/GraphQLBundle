8a56e4a81ccd8c580e0c3dfbea14847f663c5926
iarro <56431174+iarro@users.noreply.github.com>
2019-10-25 13:03:36 +0200

    fixed deprecated usage of TreeBuilder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -19,7 +19,13 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root('graphql');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('graphql');
+        }
 
         $rootNode
             ->children()
